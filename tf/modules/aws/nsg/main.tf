80c89ccc7b5690789c54bdb5b3d0dc1ab96dc9a2
Jasmit Tarang <jasmit.tarang@goteleport.com>
2026-01-12 00:52:00 -0600

    Clean up security group rules for Active Directory

--- tf/modules/aws/nsg/main.tf
+++ tf/modules/aws/nsg/main.tf
@@ -23,28 +23,6 @@ resource "aws_security_group" "nsg" {
     protocol    = "-1"          # -1 means all protocols
     cidr_blocks = ["0.0.0.0/0"] # Allow traffic to anywhere
   }
-    #  Allow Kubernetes API access from worker nodes
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Or limit it to EKS control plane IPs
-  }
-
-  # Allow worker nodes to communicate with each other
-  ingress {
-    from_port   = 1025
-    to_port     = 65535
-    protocol    = "tcp"
-    self        = true
-  }
-
-  ingress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["${var.my_external_ip}/32"] # Restrict access to the specified IP only
-  }
 
   #  Allow CoreDNS communication
   ingress {
@@ -54,27 +32,6 @@ resource "aws_security_group" "nsg" {
     self        = true
   }
 
-  # Allow all outbound traffic
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 0
-    to_port     = 0
-    protocol    = "icmp"
-  }
-
-  egress {
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 0
-    to_port     = 0
-    protocol    = "tcp"
-  }
 }
 
 resource "aws_security_group" "ad_nsg" {
@@ -101,57 +58,6 @@ resource "aws_security_group" "ad_nsg" {
     cidr_blocks = [var.vpc_cidr_block]
   }
   
-  # LDAP
-  ingress {
-    from_port   = 389
-    to_port     = 389
-    protocol    = "tcp"
-    cidr_blocks = [var.vpc_cidr_block]
-    description = "LDAP"
-  }
-
-  ingress {
-    from_port   = 389
-    to_port     = 389
-    protocol    = "udp"
-    cidr_blocks = [var.vpc_cidr_block]
-    description = "LDAP"
-  }
-
-  # Kerberos
-  ingress {
-    from_port   = 88
-    to_port     = 88
-    protocol    = "tcp"
-    cidr_blocks = [var.vpc_cidr_block]
-    description = "Kerberos"
-  }
-
-  ingress {
-    from_port   = 88
-    to_port     = 88
-    protocol    = "udp"
-    cidr_blocks = [var.vpc_cidr_block]
-    description = "Kerberos"
-  }
-
-  # Kerberos password change
-  ingress {
-    from_port   = 464
-    to_port     = 464
-    protocol    = "tcp"
-    cidr_blocks = [var.vpc_cidr_block]
-    description = "Kerberos password change"
-  }
-
-  ingress {
-    from_port   = 464
-    to_port     = 464
-    protocol    = "udp"
-    cidr_blocks = [var.vpc_cidr_block]
-    description = "Kerberos password change"
-  }
-
   # DNS
   ingress {
     from_port   = 53
@@ -169,15 +75,6 @@ resource "aws_security_group" "ad_nsg" {
     description = "DNS"
   }
 
-  # RPC Endpoint Mapper
-  ingress {
-    from_port   = 135
-    to_port     = 135
-    protocol    = "tcp"
-    cidr_blocks = [var.vpc_cidr_block]
-    description = "RPC Endpoint Mapper"
-  }
-
   # LDAP SSL
   ingress {
     from_port   = 636
@@ -187,14 +84,6 @@ resource "aws_security_group" "ad_nsg" {
     description = "LDAP SSL"
   }
 
-  # LDAP GC
-  ingress {
-    from_port   = 3268
-    to_port     = 3268
-    protocol    = "tcp"
-    cidr_blocks = [var.vpc_cidr_block]
-    description = "LDAP GC"
-  }
 
   # LDAP GC SSL
   ingress {
@@ -214,21 +103,4 @@ resource "aws_security_group" "ad_nsg" {
     description = "W32Time"
   }
 
-  # SMB
-  ingress {
-    from_port   = 445
-    to_port     = 445
-    protocol    = "tcp"
-    cidr_blocks = [var.vpc_cidr_block]
-    description = "SMB"
-  }
-
-  # RPC for LSA, SAM, NetLogon
-  ingress {
-    from_port   = 49152
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = [var.vpc_cidr_block]
-    description = "RPC for LSA, SAM, NetLogon"
-  }
 }
