b4771bd54161e3c9068418c1d1652bf0a8871596
Jasmit Tarang <jasmit.tarang@goteleport.com>
2026-06-29 20:52:56 -0500

    Update S3 key for Terraform state file

--- tf/backend.tf
+++ tf/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "jasmit-tf-state-bucket"
-    key           = "teleport-tf/terraform.state.tfstate"
+    key           = "teleport-tf/nebula-dash/terraform.state.tfstate"
     region        = "us-east-1"
     encrypt       = true
   }
